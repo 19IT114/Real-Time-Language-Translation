11aad9a40965aa2359e01ff18115a3b154670b5d
Virag Patel <19it114@charusat.edu.in>
2022-01-25 08:00:49 +0530

    Included the Flutter application files

---
+++ Flutter_Application - RTLT/android/app/src/main/kotlin/com/example/real/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.real
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter_Application - RTLT/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'screens/sign_in_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: Brightness.dark,
+      ),
+      home: SignInScreen(),
+    );
+  }
+}
